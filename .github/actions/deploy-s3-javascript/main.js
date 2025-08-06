dbd9a96c8993533939ae6a7f28b27e55fa6cf23e
ansarifaheem1986 <ansarifaheemfnu@gmail.com>
2025-08-06 16:09:06 -0500

    JavaScriptAdded

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,5 +1,16 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exec = require('@actions/exec')
+
 function run() {
+    core.notice('Hello from my custom JavaScript Action!');
 
 }
 
 run();
+
+
+/* Install the node in your system and run the below 
+npm init -y 
+npm install @action/core @actions/github @actions/exec
+then in the main.js import those packages. 
