35e8c9ec3a485b7e2beffd7bb049110ae3a7fa23
ansarifaheem1986 <ansarifaheemfnu@gmail.com>
2025-08-06 16:33:12 -0500

    JavaScripttesting

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,15 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
+    // 1) Get some input values 
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', {required: true });
+    const distFolder = core.getInput('dist-folder', {required: true });
+
+    //2) Upload the file
+    const s3Uri = `s3://${bucket}`
+    //exec.exec('aws s3 sync <local-fodler> <s3-bucket>')
+    exec.exec(`aws s3 sync ${dist-folder} ${bucket} --region ${bucketRegion}`) 
     core.notice('Hello from my custom JavaScript Action!');
 
 }
